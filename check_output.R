82a4c003df40f44eb5421875921319f8f05a7ad3
Felix Nieberding <netID@gmu.edu>
2021-11-29 10:11:06 +0100

    discard ggplotly

--- check_output.R
+++ check_output.R
@@ -8,15 +8,38 @@ library(caret)
 setwd(dir = paste("~/GFZ/_Dagow/5_data_analysis/Felix/3_gap_filling/"))
 
 # import RF Models
-mod_RF_H_2015_bagImpute <- readRDS("./RF_models/mod_RF_H_2015_bagImpute.rds")
-mod_RF_H_2015_knnImpute <- readRDS("./RF_models/mod_RF_H_2015_knnImpute.rds")
-mod_RF_H_2015_medianImpute <- readRDS("./RF_models/mod_RF_H_2015_medianImpute.rds")
+# mod_RF_H_2015_bagImpute <- readRDS("./RF_models/mod_RF_H_2015_bagImpute.rds")
+# mod_RF_H_2015_knnImpute <- readRDS("./RF_models/mod_RF_H_2015_knnImpute.rds")
+# mod_RF_H_2015_medianImpute <- readRDS("./RF_models/mod_RF_H_2015_medianImpute.rds")
+
+
+# import 
+list_of_files_T <- list.files(path = "./RF_models/",
+                              recursive = TRUE,
+                              pattern = "*1000_TRUE_wNEE.rds",
+                              full.names = TRUE)
+
+list_of_files_F <- list.files(path = "./RF_models/",
+                            recursive = TRUE,
+                            pattern = "*1000_TRUE_noNEE.rds",
+                            full.names = TRUE)
+
+mod_H_2015_T <- read_rds(list_of_files_T[1])
+mod_H_2015_F <- read_rds(list_of_files_F[1])
+
+plot(sqrt(mod_H_2015_T$finalModel$mse), ylab = "RMSE of H (W / m?)", xlab = "N trees", main = "median Impute", type = "b")
+points(sqrt(mod_H_2015_F$finalModel$mse), col = "red")
+lines(sqrt(mod_H_2015_F$finalModel$mse), col = "red")
+
+plot(varImp(mod_H_2015_T, scale = F), main = "variable importance for H \n by median Impute", xlim = c(0,100))
+plot(varImp(mod_H_2015_F, scale = F), main = "variable importance for H \n by median Impute", xlim = c(0,100))
 
 
 # import RF results
 df_RF_H_2015_bagImpute <- read_csv("./RF_results/pred_RF_H_2015_bagImpute.csv")
 df_RF_H_2015_knnImpute <- read_csv("./RF_results/pred_RF_H_2015_knnImpute.csv")
 df_RF_H_2015_medianImpute <- read_csv("./RF_results/pred_RF_H_2015_medianImpute.csv")
+
 
 
 # plot RMSE decrease for different imputation methods
