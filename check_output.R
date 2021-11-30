23414988aac0b5f6120d1c870100e9c584b86ea6
Felix Nieberding <netID@gmu.edu>
2021-11-30 16:50:24 +0000

    tidy plots

--- check_output.R
+++ check_output.R
@@ -5,38 +5,50 @@ library(tidyverse)
 library(randomForest)
 library(caret)
 library(cowplot)
-library(randomForestExplainer)
 
-setwd(dir = paste("~/GFZ/_Dagow/5_data_analysis/Felix/3_gap_filling/"))
+setwd(dir = "~/GFZ/_Dagow/5_data_analysis/Felix/3_gap_filling/")
 
 # function
-
-plot_RF_results <- function(Flux, Year) {
+plot_RF_results <- function(Flux, Year, mtry) {
   
   # import
-  mod <- read_rds(paste0("./RF_models/mod_RF_", Flux, "_", Year, "_medianImpute_750_.rds"))
+  mod <- read_rds(paste0("./RF_models/mod_RF_", Flux, "_", Year, "_medianImpute_750_mtrySet", mtry, ".rds"))
   
   # format
   df_mod <- data.frame(N_trees = 1:750, RMSE = sqrt(mod$finalModel$mse))
   
+  df_varImp <- varImp(mod, scale = F)$importance
+  df_varImp$Var <- rownames(df_varImp)
+  df_varImp <- arrange(df_varImp, Overall)
+  df_varImp$Var <- factor(df_varImp$Var, levels = as.character(df_varImp$Var))
+  
   # plot
   p1 <- ggplot(df_mod, aes(x = N_trees, y = RMSE )) +
     geom_point() +
-    labs(y = "RMSE", x = "N trees", title = paste0(Flux, " - ", Year)) +
+    labs(y = "RMSE", x = "N trees", title = paste0("Model Accuracy for ", Flux, " - ", Year)) +
     theme_bw()
   
-  p2 <- plot(varImp(mod, scale = T), main = paste0(Flux, " - ", Year), xlim = c(0,105))
+  p2 <- ggplot(df_varImp, aes(x = Overall, y = Var )) +
+    geom_segment( aes(x=0, xend=Overall, y=Var, yend=Var)) +
+    geom_point(color = "blue") +
+    labs(x = "Importance", title = paste0("Variable Importance for ", Flux, " - ", Year)) +
+    theme_classic() +
+    theme(panel.border = element_rect(fill = NA), axis.title.y = element_blank())
   
-  p3 <- plot_grid(p1,p2, ncol = 2, align = "hv", axis = "t")
+  p3 <- plot_grid(p1 + theme(text = element_text(size = 16)),
+                  p2 + theme(text = element_text(size = 16)), 
+                  ncol = 2, align = "h", axis = "t")
   
   # print
-  ggsave(paste0("./plots/", Flux, "_", Year, "_750.png"), width = 16, height = 9, dpi = 300)
+  ggsave(paste0("./plots/", Flux, "_", Year, "_750_mtrySet", mtry, ".png"), width = 16, height = 9, dpi = 300)
 }
 
 Flux = c("H", "LE", "NEE")
 Year = rep(c(2015:2021), each = length(Flux))
+mtry = rep(c(9, 10, 3), times = length(Flux))
 
-mapply(plot_RF_results, Flux = Flux, Year = Year)
+mapply(plot_RF_results, Flux = Flux, Year = Year, mtry = mtry)
 
-
-  
+Flux = "H"
+Year = 2015
+mtry = 9
